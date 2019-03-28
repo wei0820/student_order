453e954eb57262c22653690c6e6aada2f8508ff7
jheng.wei0919@gmail.com <JackPan@jackpan-MacBook-Pro.local>
2019-03-28 14:38:29 +0800

    add login activity

---
+++ app/src/main/java/com/student/student_order/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.student.student_order
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+}

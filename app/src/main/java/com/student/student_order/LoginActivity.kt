d7f72af921a7028566fe8fb3644907ec959ca9df
wei0820 <jheng.wei0919@gmail.com>
2019-04-01 17:11:48 +0800

    fix  facebook login error

--- app/src/main/java/com/student/student_order/LoginActivity.kt
+++ app/src/main/java/com/student/student_order/LoginActivity.kt
@@ -2,12 +2,33 @@ package com.student.student_order
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.facebook.CallbackManager
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
+import com.facebook.login.widget.LoginButton
+import android.content.Intent
+
+
 
 class LoginActivity : AppCompatActivity() {
+    var callbackManager: CallbackManager? = null
+    lateinit var mFbLoginBtn : LoginButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        callbackManager = CallbackManager.Factory.create()
+        AppEventsLogger.activateApp(this)
+        FacebookSdk.sdkInitialize(applicationContext)
+
         setContentView(R.layout.activity_login)
+        mFbLoginBtn = findViewById(R.id.login_button)
+        FacebookManager.printHashKey(this)
+        FacebookManager.fbLogin(this,mFbLoginBtn,callbackManager)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager!!.onActivityResult(requestCode, resultCode, data)
     }
 
 }

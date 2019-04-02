8999b0551a3b2ef6c31d00924d27b7ab193e4d8d
wei0820 <jheng.wei0919@gmail.com>
2019-04-02 14:34:33 +0800

    add checktime

--- app/src/main/java/com/student/student_order/MyApplication.kt
+++ app/src/main/java/com/student/student_order/MyApplication.kt
@@ -3,12 +3,48 @@ package com.student.student_order
 import android.app.Application
 import com.facebook.FacebookSdk
 import com.facebook.appevents.AppEventsLogger
+import com.firebase.client.Firebase
 
 class  MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         FacebookSdk.sdkInitialize(getApplicationContext())
         AppEventsLogger.activateApp(this)
+
+        Firebase.setAndroidContext(this)
+
+
+
+
+
+    }
+
+
+
+    fun  checkTime(){
+        if (MySharedPrefernces.getMyCardTime(getApplicationContext()).equals("")) {
+            System.currentTimeMillis()
+            MySharedPrefernces.saveMyCardTime(getApplicationContext(),System.currentTimeMillis().toString());
+
+
+//依照設定格式取得字串
+        } else {
+
+           var last: Long = (MySharedPrefernces.getMyCardTime(getApplicationContext())).toLong()
+           var now : Long =System.currentTimeMillis()
+            if(now-last>=24* 60 * 60 * 1000){
+                MySharedPrefernces.saveMyCardTime(getApplicationContext(),"")
+                MySharedPrefernces.saveUserKm(getApplicationContext(),"")
+                MySharedPrefernces.saveUserStep(getApplicationContext(),"")
+                MySharedPrefernces.saveUserDhot(getApplicationContext(),"")
+                MySharedPrefernces.saveUserhot(getApplicationContext(),"")
+
+            }
+
+
+        }
+
+
     }
 
 }
